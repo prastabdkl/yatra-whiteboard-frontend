77c64da558720cc278a45fc9ffcad6d24bda8478
Prastabdkl <prastabdkl@gmail.com>
2020-08-28 23:17:18 +0545

    make landing page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import { TextEditor } from "../pages";
 
 export function HomePage(props) {
-    return (
-        <div className="ui container">
-            <h3 className="ui header">Text Editor</h3>
-
-            <TextEditor></TextEditor>
-        </div>
-    );
+  return (
+    <div className="ui container">
+      <h3 className="ui header">Text Editor</h3>
+      <TextEditor></TextEditor>
+    </div>
+  );
 }
